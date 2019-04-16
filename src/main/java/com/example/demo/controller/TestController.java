b254795a3e4dc3ba5147fa0e976836c4c8671edf
xujie <805625524@qq.com>
2019-04-16 16:56:27 +0800

    飒飒

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -14,4 +14,10 @@ public class TestController {
     public String test1(){
         return "test1";
     }
+
+    @GetMapping("test2")
+    public String test2(){
+        return "test2";
+    }
+
 }
